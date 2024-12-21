ccb278ab51dd39de2f063aa439f2dd8e79688f98
Calet Devia <calebtius@gmail.com>
2024-12-20 21:45:25 -0500

    Tryed to apply the logic but not yet

--- js/calcutator.js
+++ js/calcutator.js
@@ -18,74 +18,25 @@ const $result = document.querySelector(".calculator__result");
 
 let arrayElements = [];
 
-/* asdsaasdf */
+/* Recorre los elementos operables */
 $entries.forEach((element) => {
   element.addEventListener("click", () => {
     let textElement = element.textContent.trim();
 
     arrayElements.push(textElement);
 
-    console.log();
-    if (
-      arrayElements[0] === "+" ||
-      arrayElements[0] === "/" ||
-      arrayElements[0] === "*" ||
-      arrayElements[0] === ")"
-    ) {
-      arrayElements.shift();
-      console.log(arrayElements);
-    }
-
-
-
-    let operation = arrayElements.join("");
-    console.log(7 * 2);
+    // console.log(arrayElements);
+    
+    let expretion = arrayElements.join("");
+    
+    console.log(expretion*1);
+    
+    
+    
   });
-
-  console.log(arrayElements);
 });
 
-/* Added commennt */
-
-// $numberBtns.forEach((element) => {
-//   element.addEventListener(
-//     "click",
-//     (amor = () => {
-//       let textValue = element.textContent.trim();
-//       arrayValue.push(textValue)
-//       console.log(arrayValue);
-
-//       // console.log(arrayFull);
-//       // if (arrayFull.length < 2) return;
-
-//       // return 220;
-//       // return arrayFull
-//       // return element.textContent.trim();
-//     })
-//   );
-
-// });
-
-// console.log($calculator);
-// console.log($btns);
-
-$calculator.addEventListener("submit", function () {
-  event.preventDefault();
+/* Igual */
+$equalsBtn.addEventListener("subit", () => {
+  event.defaultPrevented();
 });
-
-// $btns.forEach((element) => {
-
-//   element.addEventListener("click", () => {
-//     let valueElements = element.textContent.trim();
-//     let TogetherElements = arrayValue.join("");
-//     arrayValue.push(valueElements);
-//     // console.log(arrayValue
-//     $result.innerText = Number(TogetherElements);
-
-//     console.log(arrayValue);
-//   });
-//     console.log(element.textContent);
-// });
-
-// console.log(arrayValue);
-/* Colocar un tipo de nombre de boton diferente a los numericos, los simbolos etc. */
