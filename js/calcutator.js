28bc8f75ba5e4ee3b58faced4dfc53be9b8ce1a6
Calet Devia <calebtius@gmail.com>
2025-01-07 20:33:17 -0500

    Start again

--- js/calcutator.js
+++ js/calcutator.js
@@ -1,52 +1,35 @@
+/* Elementos del DOM*/
 const $calculator = document.querySelector(".calculator");
 
-const $numbers = document.querySelectorAll(".calculator__btn--number");
+const $display = document.querySelector(".calculator__result");
 
-const $symbols = document.querySelectorAll(".calculator__btn--symbol");
+/* Creamos el array que guarde las operaciones */
 
-const $dot = document.querySelector(".calculator__btn--dot");
+let operations = [];
 
-const $equals = document.querySelector(".calculator__btn--equals");
-
-const $entry = document.querySelectorAll(".calculator__btn--entry");
+/* Escuchamos los eventos */
 
 $calculator.addEventListener("click", (event) => {
-  let arrayValue = [];
-  let count = 0;
-  const targetNumber = event.target.classList.contains(
-    "calculator__btn--number"
-  );
+  const target = event.target.classList.contains("calculator__btn");
 
-  const numbersText = event.target.textContent.trim();
+  const valueTarget = event.target.textContent.trim();
 
-//   arrayValue.push(numbersText);
-
+  
+  operations.push(valueTarget);
+  let operationsText = operations.join("");
+  $display.innerText = operationsText;
 
 
-
-  arrayValue[0].push(numbersText);
-
-//   while (targetNumber) {
-//     count++;
-//     console.log(count);
-//   }
-
-  const targetSymbol = event.target.classList.contains(
-    "calculator__btn--symbol"
-  );
-
-  const targetSpecial = event.target.classList.contains(
-    "calculator__btn--special"
-  );
-
-  console.log(targetSymbol);
-
-  if (targetNumber) {
-    count += 1;
-
-    // console.log(count);
-
-    // console.log(inputString * 2);
-    console.log(numbersText);
+  if (valueTarget === "AC") {
+    operations = [];
+    $display.innerText = "";
   }
+
+  if (valueTarget === "C") {
+    operations.pop();
+    operations.pop();
+    operations.pop();
+    $display.innerText = operationsText;
+  }
+  console.log(operations);
 });
