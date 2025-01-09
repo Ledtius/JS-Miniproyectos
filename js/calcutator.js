8979a6266496086a1396b5da19f6f0a31af0fdda
Calet Devia <calebtius@gmail.com>
2025-01-09 18:20:49 -0500

    I feel bad

--- js/calcutator.js
+++ js/calcutator.js
@@ -6,31 +6,101 @@ const $display = document.querySelector(".calculator__result");
 /* Creamos el array que guarde las operaciones */
 
 let operations = [];
+let operation1 = [];
+let operation2 = [];
+let symbols = [];
+let dot = [];
+let minus = [];
+let cero = ["0"];
 
 /* Escuchamos los eventos */
 
 $calculator.addEventListener("click", (event) => {
   const target = event.target.classList.contains("calculator__btn");
 
+  const targetNumber = event.target.classList.contains(
+    "calculator__btn--number"
+  );
+
+  const targetOperator = event.target.classList.contains(
+    "calculator__btn--symbol"
+  );
+
+  const targetSpecial = event.target.classList.contains(
+    "calculator__btn--special"
+  );
+
   const valueTarget = event.target.textContent.trim();
 
-  operations.push(valueTarget);
-  let operationsText = operations.join("");
-  console.log(operations);
-  $display.innerText = operationsText;
+  let operationsText;
 
-  if (valueTarget === "AC") {
-    operations = [];
-    $display.innerText = "";
+  let operation1Text;
+  const regex = /^-?\d+(\.\d+)?$/;
+
+  if (targetNumber || targetSpecial) {
+     operation1.push(valueTarget);
+    operation1Text = operation1.join("");
+    
+    console.log(regex.test(operation1Text));
+
+     console.log(.43 *2)
+     console.log(1. *2)
+    if (regex.test(operation1Text)) {
+      $display.innerText = operation1Text;
+    } else {
+    }
   }
+});
 
-  if (valueTarget === "C") {
-    operations.pop();
-    operations.pop();
-
+/* 
+  if (targetNumber || targetOperator) {
+    operations.push(valueTargetNumber);
     operationsText = operations.join("");
-
     $display.innerText = operationsText;
   }
-  console.log(operations);
-});
+
+  if (targetOperator) {
+    operations.push(valueTargetOperator);
+    return;
+  }
+
+  let count = 0;
+
+  if (targetOperator) {
+    symbols.push(valueTargetOperator);
+    console.log(symbols);
+    console.log(symbols.length);
+    if (symbols.length < 2) {
+      operations.push(valueTargetOperator);
+      operationsText = operations.join("");
+      $display.innerText = operationsText;
+    }
+  } */
+
+// $calculator.addEventListener("click", (event) => {
+//   const target = event.target.classList.contains("calculator__btn");
+
+//   const valueTarget = event.target.textContent.trim();
+
+//   operations.push(valueTarget);
+//   let operationsText = operations.join("");
+//   console.log(operations);
+//   $display.innerText = operationsText;
+
+//   if (valueTarget === "AC") {
+//     operations = [];
+//     $display.innerText = "";
+//   }
+
+//   if (valueTarget === "C") {
+//     operations.pop();
+//     operations.pop();
+
+//     operationsText = operations.join("");
+
+//     $display.innerText = operationsText;
+//   }
+//   console.log(operations);
+
+//   console.log(operationsText);
+// });
