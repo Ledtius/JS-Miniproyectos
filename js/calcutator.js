d77ff6bfeb4b22c5f3c67ca5711d5b7affdebc37
Calet Devia <calebtius@gmail.com>
2025-01-12 19:45:57 -0500

    Added the validation of the operator or symbol and the second operation

--- js/calcutator.js
+++ js/calcutator.js
@@ -7,17 +7,22 @@ const $display = document.querySelector(".calculator__result");
 
 let operations = [];
 let operation1 = ["0"];
-let operation2 = [];
-let symbols = [];
+let operation2 = ["0"];
+let symbol = [];
 let dot = [];
 let minus = [];
 let cero = ["0"];
 let result;
+
 let countDot = 0;
-/* Escuchamos los eventos */
+let countDot2 = 0;
+let countSymbol = 0;
+const regex = /^[0-9]$/;
 let operation1Text = operation1.join("");
+let operation2Text = operation2.join("");
 $display.innerText = operation1Text;
 
+/* Escuchamos los eventos */
 $calculator.addEventListener("click", (event) => {
   event.preventDefault();
   const target = event.target.classList.contains("calculator__btn");
@@ -45,39 +50,89 @@ $calculator.addEventListener("click", (event) => {
   let operationsText;
   console.log();
 
-  if (targetNumber || targetDot) {
-    if (countDot > 1) {
-      countDot++;
-      console.log(countDot);
-    }
-
-    const regex = /^[0-9]$/;
-
-    operation1.push(valueTarget);
-    console.log(operation1);
-    if (regex.test(operation1[1])) {
-      console.log(operation1);
-      if (operation1[0] === "0" && regex.test(operation1[1])) {
-        operation1.shift();
-        console.log(operation1);
+  function operation(arrayValue) {
+    if (targetNumber || targetDot || targetOperator) {
+      if (targetNumber || targetDot) {
+        if (countSymbol < 1) {
+          operation1.push(valueTarget);
+          console.log(operation1);
+        }
       }
-      console.log(operation1);
-    }
 
-    if (targetDot) {
-      countDot++;
-      console.log(countDot);
+      function deleteZeros(operation) {
+        if (regex.test(operation[1])) {
+          console.log(operation);
+          if (operation[0] === "0" && regex.test(operation[1])) {
+            operation.shift();
+            console.log(operation);
+          }
+          console.log(operation);
+        }
+      }
 
-      if (countDot > 1) {
-        console.log(countDot);
-        operation1.pop();
+      deleteZeros(operation1);
+
+      function controlDot(operation) {
+        if (targetDot) {
+          countDot++;
+          console.log(countDot);
+
+          if (countDot > 1) {
+            console.log(countDot);
+            operation.pop();
+          }
+        }
+      }
+      function controlDot2(operation) {
+        if (targetDot) {
+          countDot2++;
+          console.log(countDot2);
+
+          if (countDot2 > 1) {
+            console.log(countDot2);
+            operation.pop();
+          }
+        }
+      }
+
+      controlDot(operation1);
+      if (countSymbol < 1) {
+        operation1Text = operation1.join("");
+
+        $display.innerText = operation1Text;
+      }
+      let symbolText;
+
+      console.log(countSymbol);
+      if (targetOperator) {
+        if (countSymbol < 1) {
+          countSymbol++;
+          symbol.push(valueTarget);
+          console.log(valueTarget);
+          symbolText = symbol.join("");
+
+          $display.innerText = symbolText;
+        }
+      }
+
+      if (countSymbol === 1) {
+        if (targetNumber || targetDot) {
+          operation2.push(valueTarget);
+          console.log(operation2);
+          deleteZeros(operation2);
+          controlDot2(operation2);
+          console.log(operation2);
+          operation2Text = operation2.join("");
+          $display.innerText = operation2Text;
+        }
+      }
+      switch (valueTarget) {
+        case "+":
+          eval;
       }
     }
-    // operation1.push(valueTarget);
-    operation1Text = operation1.join("");
-
-    $display.innerText = operation1Text;
   }
+  operation();
 });
 
 /* 
