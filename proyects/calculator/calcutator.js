653bd8d193876f7dfe1e3ef15f5c2010f347e7d9
Calet Devia <calebtius@gmail.com>
2025-03-09 18:01:59 -0500

    Rebuilded all of the code... Added a lot of functions like: 'save local storage' and 'get', 'create history element', 'print history element', 'delete history element', and 'copy history element'

--- proyects/calculator/calcutator.js
+++ proyects/calculator/calcutator.js
@@ -1,61 +1,213 @@
 const $calculator = document.querySelector(".calculator");
 
-const $display = document.querySelector(".calculator__result");
+const $display = document.querySelector(".calculator__result-text");
 
-let operationGeneral = "";
-let operationSpecialSymbolsGeneral = "";
-let resultGeneral;
+const $history = document.querySelector(".history__box");
 
-const operationObject = {
-  operation: "",
-  operationSpecialSymbols: "",
-  result: 0,
-};
+let stringOperation = "";
 
-let arrayObjects = [];
+let arrayOperation = getLocalStorage() || [];
 
 $calculator.addEventListener("click", (e) => {
+  let operationObject = {
+    result: "",
+    operation: "",
+  };
+
   e.preventDefault();
+
+  const textValueBtn = e.target.textContent.trim();
 
   const entries = e.target.classList.contains("calculator__btn--entry");
 
+  const clear = e.target.classList.contains("calculator__btn--clear");
+
+  const allClear = e.target.classList.contains("calculator__btn--all-clear");
+
   const equals = e.target.classList.contains("calculator__btn--equals");
 
-  const targetValue = e.target.value;
-
-  const targetTextContent = e.target.textContent.trim();
-
   if (entries) {
-    operationGeneral += targetValue;
+    if (stringOperation === "ERROR" || stringOperation === "0")
+      stringOperation = "";
 
-    operationSpecialSymbolsGeneral += targetTextContent;
+    stringOperation += textValueBtn;
+  }
 
-    $display.innerText = operationSpecialSymbolsGeneral;
+  if (clear) {
+    stringOperation = stringOperation.slice(0, -1);
+  }
+
+  if (allClear) {
+    stringOperation = "0";
   }
 
   if (equals) {
     try {
-      resultGeneral = eval(operationGeneral);
+      operationObject.operation = stringOperation;
 
-      operationObject.operation = operationGeneral;
+      stringOperation = stringOperation.replace("÷", "/");
 
-      operationObject.operationSpecialSymbols = operationSpecialSymbolsGeneral;
+      stringOperation = stringOperation.replace("×", "*");
 
-      operationObject.result = resultGeneral;
+      if (stringOperation === "") return;
 
-      arrayObjects.push(operationObject);
+      stringOperation = eval(stringOperation);
 
-      saveLocalStorage(arrayObjects);
+      operationObject.result = stringOperation;
 
-      $display.innerText = resultGeneral;
-    } catch (e) {
-      $display.innerText = "Error";
+      arrayOperation.push(operationObject);
+
+      saveLocalStorage(arrayOperation);
+      printHistoryElement();
+    } catch {
+      stringOperation = "ERROR";
+
+      // operationObject.result = operationObject;
+
+      // arrayOperation.push(operationObject);
+
+      // saveLocalStorage(arrayOperation);
     }
   }
+
+  $display.innerText = stringOperation;
 });
 
-function saveLocalStorage(arrayObjects) {
-  let arrayObjectsJSON = JSON.stringify(arrayObjects);
+function saveLocalStorage(arrayOperation) {
+  let JSONArrayObject = JSON.stringify(arrayOperation);
 
-  localStorage.setItem("arrayObjects", arrayObjectsJSON);
+  localStorage.setItem("arrayOperationObject", JSONArrayObject);
 }
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("arrayOperationObject")) || [];
+}
+
+function createHistoryElement(operation, result, index) {
+  const $element = document.createElement("div");
+
+  $element.className = `history__element`;
+  $element.id = `element${index}`;
+
+  const $strong = document.createElement("strong");
+
+  $strong.className = "history__operation";
+
+  $strong.textContent = `${operation} = ${result}`;
+
+  $element.append($strong);
+
+  const $historyBtnMessage = document.createElement("div");
+
+  $historyBtnMessage.className = "history__btns-message";
+
+  $element.append($historyBtnMessage);
+
+  const $historyBtns = document.createElement("div");
+
+  $historyBtns.className = "history__btns";
+
+  $historyBtnMessage.append($historyBtns);
+
+  const $copyBtn = document.createElement("button");
+
+  $copyBtn.className = "history__btn history__btn--copy";
+
+  $copyBtn.innerHTML = ` <svg
+                    class="history__icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                  >
+                    <path
+                      d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z"
+                    />
+                  </svg>`;
+
+  $historyBtns.append($copyBtn);
+
+  const $deleteBtn = document.createElement("button");
+
+  $deleteBtn.className = "history__btn history__btn--delete";
+
+  $deleteBtn.innerHTML = `                  <svg
+                    class="history__icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512"
+                  >
+                    <path
+                      d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z"
+                    />
+                  </svg>
+`;
+
+  $historyBtns.append($deleteBtn);
+
+  const $btnsMessage = document.createElement("div");
+
+  $btnsMessage.className = "btns__message";
+
+  $historyBtnMessage.append($btnsMessage);
+
+  $history.append($element);
+
+  deleteHistoryElement($deleteBtn, $element, $btnsMessage);
+
+  copyHistoryElement($copyBtn, $element, $btnsMessage);
+}
+
+function printHistoryElement() {
+  $history.innerHTML = "";
+
+  arrayOperation = getLocalStorage();
+
+  if (arrayOperation.length === 0) return;
+
+  console.log(arrayOperation);
+
+  arrayOperation.forEach((element, index) => {
+    createHistoryElement(element.operation, element.result, index);
+  });
+}
+
+function deleteHistoryElement($deleteBtn, $element, $btnMessage) {
+  $deleteBtn.addEventListener("click", () => {
+    arrayOperation = getLocalStorage();
+
+    let index = Number($element.id.match(/-?\d+/g).join(""));
+
+    console.log(typeof index);
+    $btnMessage.style = "color: #ed642b";
+    $btnMessage.innerText = "Eliminado";
+
+    setTimeout(() => {
+      $btnMessage.innerText = "";
+      arrayOperation = arrayOperation.filter((_, index2) => index2 !== index);
+      console.log(arrayOperation[index]);
+
+      saveLocalStorage(arrayOperation);
+
+      printHistoryElement();
+    }, 500);
+
+    // $element.id.remove();
+  });
+}
+
+function copyHistoryElement($copyBtn, $element, $btnMessage) {
+  $copyBtn.addEventListener("click", () => {
+    const textOperation = $element.textContent.trim();
+
+    console.log(textOperation);
+
+    $btnMessage.style = "color: #049541";
+    $btnMessage.innerText = "Copiado!";
+
+    setTimeout(() => {
+      $btnMessage.innerText = "";
+    }, 500);
+
+    navigator.clipboard.writeText(textOperation);
+  });
+}
+
+printHistoryElement();
