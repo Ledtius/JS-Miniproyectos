29795c98acfd6e8668ab3e82371ccc218209e92b
Calet Devia <calebtius@gmail.com>
2025-03-09 10:38:12 -0500

    Deleted extract value function, added, new variables, condition entries, and equals. We can operate entry values and print them on the display. Added try and catch to prevent errors with eval(), and finally add save local storage function

--- proyects/calculator/calcutator.js
+++ proyects/calculator/calcutator.js
@@ -1,21 +1,61 @@
 const $calculator = document.querySelector(".calculator");
 
-const $entries = document.querySelectorAll(".calculator__btn--entry");
+const $display = document.querySelector(".calculator__result");
+
+let operationGeneral = "";
+let operationSpecialSymbolsGeneral = "";
+let resultGeneral;
+
+const operationObject = {
+  operation: "",
+  operationSpecialSymbols: "",
+  result: 0,
+};
+
+let arrayObjects = [];
 
 $calculator.addEventListener("click", (e) => {
   e.preventDefault();
+
   const entries = e.target.classList.contains("calculator__btn--entry");
 
-  extractValue($entries);
+  const equals = e.target.classList.contains("calculator__btn--equals");
+
+  const targetValue = e.target.value;
+
+  const targetTextContent = e.target.textContent.trim();
+
+  if (entries) {
+    operationGeneral += targetValue;
+
+    operationSpecialSymbolsGeneral += targetTextContent;
+
+    $display.innerText = operationSpecialSymbolsGeneral;
+  }
+
+  if (equals) {
+    try {
+      resultGeneral = eval(operationGeneral);
+
+      operationObject.operation = operationGeneral;
+
+      operationObject.operationSpecialSymbols = operationSpecialSymbolsGeneral;
+
+      operationObject.result = resultGeneral;
+
+      arrayObjects.push(operationObject);
+
+      saveLocalStorage(arrayObjects);
+
+      $display.innerText = resultGeneral;
+    } catch (e) {
+      $display.innerText = "Error";
+    }
+  }
 });
 
-function extractValue($HTMLElements) {
-  $HTMLElements.forEach(($element, index) => {
-    $element.addEventListener("click", (e) => {
-      e.stopPropagation();
+function saveLocalStorage(arrayObjects) {
+  let arrayObjectsJSON = JSON.stringify(arrayObjects);
 
-      console.log($element.value);
-    });
-  });
+  localStorage.setItem("arrayObjects", arrayObjectsJSON);
 }
-
