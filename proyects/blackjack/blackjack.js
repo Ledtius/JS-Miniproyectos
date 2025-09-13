b4767ae3856be4ee1d40e029a5dddcd7cb90c4c4
Calet Devia <calebtius@gmail.com>
2025-09-13 07:38:59 -0500

    Fixed the first for loop and added another one

--- proyects/blackjack/blackjack.js
+++ proyects/blackjack/blackjack.js
@@ -2,14 +2,27 @@ let deck = [];
 
 const createDeck = () => {
   const letters = ["C", "D", "H", "S"];
+  const letters2 = ["J", "K", "Q"];
   let count = 0;
+  let count2 = 0;
 
-  for (let index = 2; index <= 10; index++) {
-    if (index <= 9) deck.push(`${index}${letters[count]}`);
+  for (let index = 2; index <= 11; index++) {
+    if (index <= 10) deck.push(`${index}${letters[count]}`);
     else {
       count++;
       if (count !== letters.length) index = 1;
     }
+  }
+
+  for (let index = 0; index <= letters.length; index++) {
+    if (index <= letters.length - 1)
+      deck.push(`${letters2[count2]}${letters[index]}`);
+    else {
+      count2++;
+      console.log(index);
+      if (count2 !== letters2.length) index = -1;
+    }
+    console.log(index);
   }
 
   /* ssss */
