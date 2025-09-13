667d644bd70471b772044a62f1ab287ce5a2cdea
Calet Devia <calebtius@gmail.com>
2025-09-13 07:03:45 -0500

    Fixed the creation of the type of the cards (code more maintainable)

--- proyects/blackjack/blackjack.js
+++ proyects/blackjack/blackjack.js
@@ -1,20 +1,25 @@
 let deck = [];
 
 const createDeck = () => {
-  let count = 0;
   const letters = ["C", "D", "H", "S"];
-  for (let index = 2; index <= 9; index++) {
-    deck.push(`${index}${letters[count]}`);
-    count++;
-    if (count === letters.length) count = 0;
+  let count = 0;
+
+  for (let index = 2; index <= 10; index++) {
+    if (index <= 9) deck.push(`${index}${letters[count]}`);
+    else {
+      count++;
+      if (count !== letters.length) index = 1;
+    }
   }
 
   /* ssss */
   /* 
+      deck.push(`${index}S`);
     deck.push(`${index}C`);
     deck.push(`${index}D`);
     deck.push(`${index}H`);
-    deck.push(`${index}S`);
+
+
 */
 
   console.table(deck);
