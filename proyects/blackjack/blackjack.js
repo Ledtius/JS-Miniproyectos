fbb427348f599e9bb368a71f0e366701c0863b00
Calet Devia <calebtius@gmail.com>
2025-09-13 10:33:33 -0500

    Added the underscore and the ._shuffle method in the code

--- proyects/blackjack/blackjack.js
+++ proyects/blackjack/blackjack.js
@@ -4,27 +4,6 @@ const createDeck = () => {
   const types = ["C", "D", "H", "S"];
   const specials = ["A", "J", "K", "Q"];
 
-  /* Equivalent */
-  // let count = 0;
-  // let count2 = 0;
-
-  // for (let index = 2; index <= 11; index++) {
-  //   if (index <= 10) deck.push(`${index}${types[count]}`);
-  //   else {
-  //     count++;
-  //     if (count !== types.length) index = 1;
-  //   }
-  // }
-
-  // for (let index = 0; index <= types.length; index++) {
-  //   if (index <= types.length - 1)
-  //     deck.push(`${specials[count2]}${types[index]}`);
-  //   else {
-  //     count2++;
-  //     if (count2 !== specials.length) index = -1;
-  //   }
-  // }
-
   for (let index = 2; index <= 10; index++) {
     for (const type of types) {
       deck.push(index + type);
@@ -38,6 +17,10 @@ const createDeck = () => {
   }
 
   console.table(deck);
+  deck = _.shuffle(deck);
+  
+
+  console.log(deck);
 };
 
 createDeck();
